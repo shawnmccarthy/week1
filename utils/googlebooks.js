9c823ecb0c7802f89e808bb8931b042a962d9daa
bspacek <74202923+bspacek@users.noreply.github.com>
2021-01-31 11:11:01 -0700

    Spacek HW0

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -8,7 +8,15 @@ module.exports = async (phrase) => {
         }
     })
 
-    return JSON.stringify(results.data);
+    // return JSON.stringify(results.data);
+    let customObject = {
+        data: results.data,
+            status: results.status,
+            statusText: results.statusText,
+            headers: results.headers,
+            requestHeader: results.config.headers
+    }
+    return customObject;
 }
 
 /*  console.log(results.data);
